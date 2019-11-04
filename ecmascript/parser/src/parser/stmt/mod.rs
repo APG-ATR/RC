45af1a7c1e79c5f817a4e821eb315a2535ab74fe
강동윤 <kdy1997.dev@gmail.com>
2019-11-04 15:40:22 +0900

    Don't emit errors related to first parameter if the length of the parameters is not 1

--- ecmascript/parser/src/parser/stmt/mod.rs
+++ ecmascript/parser/src/parser/stmt/mod.rs
@@ -787,25 +787,28 @@ impl<'a, I: Tokens> Parser<'a, I> {
                     for d in decl.decls.iter().skip(1) {
                         self.emit_err(d.name.span(), SyntaxError::TooManyVarInForInHead);
                     }
-                }
-                if decl.decls[0].init.is_some() {
-                    self.emit_err(
-                        decl.decls[0].name.span(),
-                        SyntaxError::VarInitializerInForInHead,
-                    );
-                }
+                } else {
+                    if decl.decls[0].init.is_some() {
+                        self.emit_err(
+                            decl.decls[0].name.span(),
+                            SyntaxError::VarInitializerInForInHead,
+                        );
+                    }
 
-                let type_ann = match decl.decls[0].name {
-                    Pat::Ident(ref v) => Some(&v.type_ann),
-                    Pat::Array(ref v) => Some(&v.type_ann),
-                    Pat::Assign(ref v) => Some(&v.type_ann),
-                    Pat::Rest(ref v) => Some(&v.type_ann),
-                    Pat::Object(ref v) => Some(&v.type_ann),
-                    _ => None,
-                };
-                if let Some(type_ann) = type_ann {
-                    if type_ann.is_some() {
-                        self.emit_err(decl.decls[0].name.span(), SyntaxError::TS2483);
+                    {
+                        let type_ann = match decl.decls[0].name {
+                            Pat::Ident(ref v) => Some(&v.type_ann),
+                            Pat::Array(ref v) => Some(&v.type_ann),
+                            Pat::Assign(ref v) => Some(&v.type_ann),
+                            Pat::Rest(ref v) => Some(&v.type_ann),
+                            Pat::Object(ref v) => Some(&v.type_ann),
+                            _ => None,
+                        };
+                        if let Some(type_ann) = type_ann {
+                            if type_ann.is_some() {
+                                self.emit_err(decl.decls[0].name.span(), SyntaxError::TS2483);
+                            }
+                        }
                     }
                 }
 
