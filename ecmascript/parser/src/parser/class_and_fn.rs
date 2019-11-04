7e28ed9b1ebb12466b913c21822c77e1f26cd386
강동윤 <kdy1997.dev@gmail.com>
2019-11-04 12:48:22 +0900

    TS1105: Deny break in functions

--- ecmascript/parser/src/parser/class_and_fn.rs
+++ ecmascript/parser/src/parser/class_and_fn.rs
@@ -645,6 +645,7 @@ impl<'a, I: Tokens> Parser<'a, I> {
         let ctx = Context {
             in_async: is_async,
             in_generator: is_generator,
+            is_break_continue_allowed: false,
             ..self.ctx()
         };
 
