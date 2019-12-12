925758a662cdf5e1790006863796a02d565e34e0
강동윤 <kdy1997.dev@gmail.com>
2019-12-12 22:53:38 +0900

    Parse options.json

--- ecmascript/polyfills/tests/test.rs
+++ ecmascript/polyfills/tests/test.rs
@@ -1,12 +1,128 @@
 #![feature(try_blocks)]
+#![feature(test)]
 
-use std::{env, io, path::PathBuf, process::Command};
+extern crate test;
+
+use serde::Deserialize;
+use serde_json::Value;
+use std::{collections::HashMap, env, fs::File, io, path::PathBuf, process::Command};
 use swc_common::FromVariant;
+use test::{TestDesc, TestDescAndFn};
+use walkdir::WalkDir;
+
+/// options.json file
+#[derive(Debug, Deserialize)]
+struct BabelOptions {
+    presets: Vec<(String, PresetConfig)>,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+struct PresetConfig {
+    #[serde(default)]
+    pub use_built_ins: UseBuiltIns,
+
+    #[serde(default)]
+    pub corejs: CoreJs,
+
+    #[serde(default)]
+    pub modules: ModulesConfig,
+
+    #[serde(default)]
+    pub targets: HashMap<String, Value>,
+
+    #[serde(default)]
+    pub include: Vec<String>,
+
+    #[serde(default)]
+    pub exclude: Vec<String>,
+
+    #[serde(default)]
+    pub force_all_transforms: bool,
+
+    #[serde(default)]
+    pub shippedProposals: bool,
+
+    #[serde(default)]
+    pub config_path: String,
+
+    #[serde(default)]
+    pub debug: bool,
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(deny_unknown_fields, rename_all = "camelCase")]
+#[serde(untagged)]
+pub enum CoreJs {
+    Ver(usize),
+    VerWithMinor(String),
+    Val(HashMap<String, Value>),
+}
+
+impl Default for CoreJs {
+    fn default() -> Self {
+        Self::Ver(2)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum ModulesConfig {
+    Bool(bool),
+}
+
+impl Default for ModulesConfig {
+    fn default() -> Self {
+        ModulesConfig::Bool(false)
+    }
+}
+
+#[derive(Debug, Deserialize)]
+#[serde(untagged)]
+enum UseBuiltIns {
+    Bool(bool),
+    Str(String),
+}
+
+impl Default for UseBuiltIns {
+    fn default() -> Self {
+        UseBuiltIns::Bool(false)
+    }
+}
 
 #[derive(Debug, FromVariant)]
 enum Error {
     Io(io::Error),
     Var(env::VarError),
+    WalkDir(walkdir::Error),
+    Json(serde_json::Error),
+}
+
+fn load() -> Result<Vec<TestDescAndFn>, Error> {
+    let mut tests = vec![];
+    let mut dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+    dir.push("tests");
+    dir.push("fixtures");
+
+    for entry in WalkDir::new(dir) {
+        let e = entry?;
+
+        if e.metadata()?.is_file() {
+            continue;
+        }
+
+        match e.path().join("input.mjs").metadata() {
+            Ok(e) if e.is_file() => {}
+            _ => continue,
+        }
+
+        let cfg: BabelOptions =
+            serde_json::from_reader(File::open(e.path().join("options.json"))?)?;
+        assert_eq!(cfg.presets.len(), 1);
+        let cfg = cfg.presets.into_iter().map(|v| v.1).next().unwrap();
+    }
+
+    Ok(tests)
 }
 
 fn exec() {
@@ -20,4 +136,6 @@ fn exec() {
 }
 
 #[test]
-fn fixtures() {}
+fn fixtures() {
+    load().expect("failed to load fixtures");
+}
