ec78b1a839e0acc0425826c2f887b1e07c32edb9
강동윤 <kdy1997.dev@gmail.com>
2019-12-12 22:03:05 +0900

    Install browserlist

--- ecmascript/polyfills/tests/test.rs
+++ ecmascript/polyfills/tests/test.rs
@@ -1,1 +1,23 @@
+#![feature(try_blocks)]
 
+use std::{env, io, path::PathBuf, process::Command};
+use swc_common::FromVariant;
+
+#[derive(Debug, FromVariant)]
+enum Error {
+    Io(io::Error),
+    Var(env::VarError),
+}
+
+fn exec() {
+    let res: Result<_, Error> = try {
+        let mut query_js = PathBuf::from(env::var("CARGO_MANIFEST_DIR")?);
+        query_js.push("tests");
+        query_js.push("query.js");
+
+        Command::new("node").arg(query_js).status()?
+    };
+}
+
+#[test]
+fn fixtures() {}
