932c4e61c8ef99b5b36010b497ee96367acf3e41
강동윤 <kdy1997.dev@gmail.com>
2019-12-16 23:10:54 +0900

    Use alias

--- ecmascript/preset_env/src/corejs2/builtin.rs
+++ ecmascript/preset_env/src/corejs2/builtin.rs
@@ -1,10 +1,10 @@
-use crate::{parse_version, BrowserData};
+use crate::{parse_version, BrowserData, Versions};
 use hashbrown::HashMap;
 use once_cell::sync::Lazy;
 use semver::Version;
 use swc_atoms::JsWord;
 
-pub(crate) static BUILTINS: Lazy<HashMap<String, BrowserData<Option<Version>>>> = Lazy::new(|| {
+pub(crate) static BUILTINS: Lazy<HashMap<String, Versions>> = Lazy::new(|| {
     let map: HashMap<_, BrowserData<Option<String>>> =
         serde_json::from_str(include_str!("builtin.json")).expect("failed to parse json");
 
