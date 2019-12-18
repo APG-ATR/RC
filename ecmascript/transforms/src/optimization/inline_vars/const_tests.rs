232ada42b8ade7746a73eda68d4db1de0a4cf478
강동윤 <kdy1997.dev@gmail.com>
2019-12-18 14:08:45 +0900

    Add constant tests

--- ecmascript/transforms/src/optimization/inline_vars/const_tests.rs
+++ ecmascript/transforms/src/optimization/inline_vars/const_tests.rs
@@ -2,7 +2,7 @@
 
 use super::inline_vars;
 
-fn fold(src: &str, expected: &str) {
+fn test(src: &str, expected: &str) {
     test_transform!(
         ::swc_ecma_parser::Syntax::default(),
         |_| inline_vars(),
@@ -13,8 +13,8 @@ fn fold(src: &str, expected: &str) {
 }
 
 /// Should not modify expression.
-fn fold_same(s: &str) {
-    fold(s, s)
+fn test_same(s: &str) {
+    test(s, s)
 }
 
 #[test]
@@ -25,23 +25,23 @@ fn test_inline_variables_constants() {
     test("var AA=1", "");
     test("var AA; AA=1", "1");
     test("var AA; if (false) AA=1; AA;", "if (false) 1; 1;");
-    testSame("var AA; if (false) AA=1; else AA=2; AA;");
+    test_same("var AA; if (false) AA=1; else AA=2; AA;");
 
     // Make sure that nothing explodes if there are undeclared variables.
-    testSame("var x = AA;");
+    test_same("var x = AA;");
 
     // Don't inline if it will make the output larger.
-    testSame("var AA = '1234567890'; foo(AA); foo(AA); foo(AA);");
+    test_same("var AA = '1234567890'; foo(AA); foo(AA); foo(AA);");
 
     test("var AA = '123456789012345';AA;", "'123456789012345'");
 }
 
 #[test]
 fn test_no_inline_arrays_or_regexps() {
-    testSame("var AA = [10,20]; AA[0]");
-    testSame("var AA = [10,20]; AA.push(1); AA[0]");
-    testSame("var AA = /x/; AA.test('1')");
-    testSame("/** @const */ var aa = /x/; aa.test('1')");
+    test_same("var AA = [10,20]; AA[0]");
+    test_same("var AA = [10,20]; AA.push(1); AA[0]");
+    test_same("var AA = /x/; AA.test('1')");
+    test_same("/** @const */ var aa = /x/; aa.test('1')");
 }
 
 #[test]
@@ -70,29 +70,7 @@ fn test_inline_conditionally_defined_constant3() {
 }
 
 #[test]
-fn test_inline_defined_constant() {
-    test(
-        "/**\n"
-            + " * @define {string}\n"
-            + " */\n"
-            + "var aa = '1234567890';\n"
-            + "foo(aa); foo(aa); foo(aa);",
-        "foo('1234567890');foo('1234567890');foo('1234567890')",
-    );
-
-    test(
-        "/**\n"
-            + " * @define {string}\n"
-            + " */\n"
-            + "var ABC = '1234567890';\n"
-            + "foo(ABC); foo(ABC); foo(ABC);",
-        "foo('1234567890');foo('1234567890');foo('1234567890')",
-    );
-}
-
-#[test]
 fn test_inline_variables_constants_with_inline_all_strings_on() {
-    inlineAllStrings = true;
     test(
         "var AA = '1234567890'; foo(AA); foo(AA); foo(AA);",
         "foo('1234567890'); foo('1234567890'); foo('1234567890')",
@@ -101,7 +79,7 @@ fn test_inline_variables_constants_with_inline_all_strings_on() {
 
 #[test]
 fn test_no_inline_without_const_declaration() {
-    testSame("var abc = 2; var x = abc;");
+    test_same("var abc = 2; var x = abc;");
 }
 
 // TODO(nicksantos): enable this again once we allow constant aliasing.
@@ -116,6 +94,6 @@ fn test_inline_constant_alias() {
 
 #[test]
 fn test_no_inline_aliases() {
-    testSame("var XXX = new Foo(); var yyy = XXX; bar(yyy)");
-    testSame("var xxx = new Foo(); var YYY = xxx; bar(YYY)");
+    test_same("var XXX = new Foo(); var yyy = XXX; bar(yyy)");
+    test_same("var xxx = new Foo(); var YYY = xxx; bar(YYY)");
 }
