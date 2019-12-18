084da77a6135a08752e000492c94bd4788c22f90
강동윤 <kdy1997.dev@gmail.com>
2019-12-19 00:17:45 +0900

    lint

--- ecmascript/transforms/src/optimization/simplify/dce/mod.rs
+++ ecmascript/transforms/src/optimization/simplify/dce/mod.rs
@@ -487,10 +487,10 @@ impl Fold<Expr> for Remover<'_> {
 
         match e {
             Expr::Assign(AssignExpr {
-                span,
                 op: op!("="),
                 left: PatOrExpr::Pat(box Pat::Ident(ref l)),
                 right: box Expr::Ident(r),
+                ..
             }) if l.sym == r.sym && l.span.ctxt() == r.span.ctxt() => return Expr::Ident(r),
 
             Expr::Assign(AssignExpr {
@@ -560,10 +560,10 @@ fn ignore_result(e: Expr) -> Option<Expr> {
         Expr::Paren(ParenExpr { expr, .. }) => ignore_result(*expr),
 
         Expr::Assign(AssignExpr {
-            span,
             op: op!("="),
             left: PatOrExpr::Pat(box Pat::Ident(ref l)),
             right: box Expr::Ident(r),
+            ..
         }) if l.sym == r.sym && l.span.ctxt() == r.span.ctxt() => None,
 
         Expr::Bin(BinExpr {
