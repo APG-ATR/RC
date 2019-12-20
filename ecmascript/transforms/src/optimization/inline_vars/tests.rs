b16d33789510cbaa1a069f870b3aa707dc5de81f
강동윤 <kdy1997.dev@gmail.com>
2019-12-20 15:57:08 +0900

    Run resolver()

--- ecmascript/transforms/src/optimization/inline_vars/tests.rs
+++ ecmascript/transforms/src/optimization/inline_vars/tests.rs
@@ -1,11 +1,13 @@
 //! Copied from https://github.com/google/closure-compiler/blob/6ca3b62990064488074a1a8931b9e8dc39b148b3/test/com/google/javascript/jscomp/InlineVariablesTest.java
 
 use super::{inline_vars, Config};
+use crate::resolver;
+use swc_common::chain;
 
 fn test_cfg(c: Config, src: &str, expected: &str) {
     test_transform!(
         ::swc_ecma_parser::Syntax::default(),
-        |_| inline_vars(c),
+        |_| chain!(resolver(), inline_vars(c)),
         src,
         expected,
         true
