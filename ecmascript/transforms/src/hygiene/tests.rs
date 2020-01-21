101875721bbb6eaa6a391d78357c80f1b0be8709
강동윤 <kdy1997.dev@gmail.com>
2020-01-21 21:55:59 +0900

    Add a test

--- ecmascript/transforms/src/hygiene/tests.rs
+++ ecmascript/transforms/src/hygiene/tests.rs
@@ -1164,3 +1164,30 @@ fn exported_class_1() {
         export { Foo1 as Foo };",
     );
 }
+
+#[test]
+fn issue_598() {
+    test_module(
+        |tester| {
+            let mark1 = Mark::fresh(Mark::root());
+            let mark2 = Mark::fresh(Mark::root());
+
+            Ok(tester
+                .parse_module(
+                    "actual1.js",
+                    "export function foo() {
+    console.log(i18n(_templateObject()));
+    console.log(i18n(_templateObject()));
+}",
+                )?
+                .fold_with(&mut OnceMarker::new(&[(
+                    "_templateObject",
+                    &[mark1, mark2],
+                )])))
+        },
+        "export function foo() {
+    console.log(i18n(_templateObject()));
+    console.log(i18n(_templateObject1()));
+}",
+    );
+}
